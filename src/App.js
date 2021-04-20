be0c2144ad3781e8ce9efbaa5766eb63a08026e7
Kelli Landry <kelli.landry@perficient.com>
2021-04-20 14:53:48 -0500

    Add photos, modify hero, add herostyles file, figure out img display issue.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './static/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
