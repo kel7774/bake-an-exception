b31a7a61db04c980c6da2b7b5396679b13555a7e
Kelli Landry <kelli.landry@perficient.com>
2021-04-28 12:39:30 -0500

    added react-share, no insta button, solution later

--- src/static/Footer.js
+++ src/static/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FacebookIcon, InstapaperIcon } from 'react-share';
 import FooterStyles from '../styles/FooterStyles';
 
 function Footer() {
+    const facebook = `https://www.facebook.com/BakeAnException`;
     return (
         <FooterStyles>
             <div className="content">
@@ -10,6 +12,12 @@ function Footer() {
                 </div>
                 <div>
                     Follow Us
+                    <div>
+                        <a href="https://www.facebook.com/BakeAnException" target="_blank" >
+                            <FacebookIcon size={15} round={true} />
+                            <InstapaperIcon size={15} round={true} />
+                        </a>
+                    </div>
                 </div>
                 <div>
                     Contact
