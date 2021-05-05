62037c432f924d6778c020b58cd9bac3543d6646
Kelli Landry <landry.kel@gmail.com>
2021-05-05 09:59:54 -0500

    Add more fields to cupcake form, work on contactus form.

--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { sendForm } from 'emailjs-com';
 import { useForm } from 'react-hook-form';
 import BasicInfo from '../forms/BasicInfo';
 
-function ContactForm({ register, errors }) {
+function ContactForm() {
+    const { register, formState: { errors } } = useForm();
+    const {firstName, setFirstName} = useState(undefined);
+    const handleFirstName = (e) => {
+        e.preventDefault();
+        setFirstName(e.target.value)
+    }
     return (
         <div>
             <form>
-                
+                <div className="fieldLabel">
+                <div className="fieldLabel">
+                    <label htmlFor='firstName'>First Name</label>
+                    <input 
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name"
+                        value={firstName} 
+                        onChange={(e) => handleFirstName(e)}
+                        {...register("firstName", { required: true })}
+                    />
+                    {errors.firstName && "First name is required."}
+                    </div>
+                </div>
+                <div className="fieldLabel">
+                    <label htmlFor="Last Name">Last Name</label>   
+                    <input 
+                        name="Last Name"
+                        placeholder="Last Name"
+                        type="text"
+                        {...register("Last Name", { required: true })}
+                    />
+                    {errors.lastName && "Must enter in Last Name."}
+                </div>
+                <div className="fieldLabel">
+                    <label htmlFor="Email">Email</label>
+                    <input 
+                        name="Email"
+                        placeholder="Email"
+                        type="email"
+                        {...register("Email", { required: true })}
+                    />
+                </div>
+                <div className="fieldLabel">
+                    <label htmlFor="Phone Number">Phone Number</label>
+                    <input 
+                        name="Phone Number"
+                        placeholder="Phone Number"
+                        type="tel"
+                        {...register("Phone Number", { required: true })}
+                    />
+                </div>
             </form>
         </div>
     )
--- src/components/forms/CupcakeForm.js
+++ src/components/forms/CupcakeForm.js
@@ -16,7 +16,7 @@ function CupcakeForm ({ register, errors, handleFillings, handleQuantity }) {
                     <option value="3Dozen">3 Dozen - 36 Cupcakes</option>
                     <option value="3Dozen">4 Dozen - 48 Cupcakes</option>
                 </select>
-                {errors.tiers && "Must select number of fillings"}
+                {errors.quantity && "Must select a quantity."}
             </div>
             <div className="fieldLabel">
                 <label htmlFor="fillings">How many fillings?</label>
@@ -29,7 +29,59 @@ function CupcakeForm ({ register, errors, handleFillings, handleQuantity }) {
                     <option value="1">1 Filling</option>
                     <option value="2">2 Fillings(1/2 & 1/2)</option>
                 </select>
-                {errors.tiers && "Must select number of fillings"}
+                {errors.tiers && "Must select number of fillings."}
+            </div>
+            <div className="fieldLabel">
+                <label htmlFor="fillings-first">Choose First Filling:</label>
+                <select 
+                   name="filling-1"
+                    id="filling-1"
+                    {...register("filling-1", { required: true })} 
+                    onChange={(e) => handleFillings(e)}
+                >
+                    <option value="no-filling">No Filling</option>
+                    <option value="lemon">Lemon</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="bavarian-creme">Bavarian Creme</option>
+                    <option value="choc-bavarian">Chocolate Bavarian Creme</option>
+                    <option value="raspberry">Raspberry</option>
+                    <option value="blueberry">Blueberry</option>
+                    <option value="cream-cheese">Cream Cheese</option>
+                </select>
+                {errors.filling1 && "Must select a filling."}
+            </div>
+            <div className="fieldLabel">
+                <label htmlFor="fillings-second">Choose Second Filling:</label>
+                <select 
+                   name="filling2"
+                    id="filling2"
+                    {...register("filling2", { required: true })} 
+                    onChange={(e) => handleFillings(e)}
+                >
+                    <option value="no-filling">No Filling</option>
+                    <option value="lemon">Lemon</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="bavarian-creme">Bavarian Creme</option>
+                    <option value="choc-bavarian">Chocolate Bavarian Creme</option>
+                    <option value="raspberry">Raspberry</option>
+                    <option value="blueberry">Blueberry</option>
+                    <option value="cream-cheese">Cream Cheese</option>
+                </select>
+                {errors.filling2 && "Must select a filling."}
+            </div>
+            <div className="fieldLabel">
+                <label htmlFor="icing">Choose Icing:</label>
+                <select
+                    name="icing"
+                    id="icing"
+                    {...register("icing", { required: true })}
+                    onChange={(e) => {}}
+                >
+                    <option value="choc-buttercream">Chocolate Buttercream</option>
+                    <option value="vanilla-buttercream">Vanilla Buttercream</option>
+                    <option value="marshmallow">Marshmallow</option>
+                </select>
+                {errors.icing && "Must select an icing."}
             </div>
             <p>**Cupcakes must be ordered by the dozen at minimum.</p>
         </div>
